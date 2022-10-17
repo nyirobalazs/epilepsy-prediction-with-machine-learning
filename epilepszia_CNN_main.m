0e00bb060a31fb97caae0ba76134543743654753
Balazs Nyiro <balasz1995@gmail.com>
2022-10-18 00:29:18 +0200

    Update epilepszia_CNN_main.m

--- epilepszia_CNN_main.m
+++ epilepszia_CNN_main.m
@@ -1,26 +1,24 @@
 %%%
-% A program fuuttatásához szükség van:
+% Required to run the program:
 % - Deep Learning Toolbox Model for GoogLeNet Network
 % - Wavelet Toolbox
 % - Deep Learning toolbox
-% ...kiegészítőkre
-% Továbbá hogy az összes projekt file egy könyvtárban legyen és utánna
-% nyissuk meg a main könyvtárat
+% ... accessories:
+% Furthermore, all project files must be in one directory and then open the main directory
 %%%
-%% Adatok betöltése
 
-%betöltjük az egerekről készült EEG felvételeket
+%% Load data
+%load EEG recordings from mice
 load dbNew.mat
 
-%Összes EEG trial egy 3D-s (csatorna,idő,trial) mátrixba összesítése
+%Aggregation of all EEG trials into a 3D (channel,time,trial) matrix
 for i=1:102
     EEG(:,:,i) = db{1,i}.eeg;
 end
 
-%% Time-Frequency Reprezentáció készítése
-% tesztelési célra, hogy lássuk korrekt-e az elképzelés, egy-egy általunk
-% választott normál(~n),roham(ictal~i) és roham utáni(postictal~p)
-% szakaszon
+%% Create Time-Frequency Representation
+% for testing purposes, to see if the idea is correct, for one of our
+% normal(~n), seizure(ictal~i) and post-seizure(postictal~p) phase
 
 Fs = 1000;
 fb = cwtfilterbank('SignalLength',10000,...
@@ -49,36 +47,35 @@ set(gca,'yscale','log');shading interp;axis tight;
 title('Post-ictal');xlabel('Idő (sec)');ylabel('Frekvencia (Hz)')
 
 
-%% Adatok előkészítése
+%% Pre-processing of data
 
-%Adatok dimenzióinak kinyerése
+%Data dimensions extraction
 [numb_chen,pnts,trials]=size(EEG);
 
-%EEG roham minták kiválasztott csatornáinak(channels) feldarabolása normál,
-%ictal és postictal szakaszokra. Szétválasztása training és validation
-%adatokra(90-10%-os arányban),majd tovább osztása a targetLength-el
-%megyegyező méretűre
+%EEG seizure samples of selected channels are split into normal channels,
+%ictal and postictal sections. Separation training and validation
+%data (90-10% ratio) and further division by targetLength into a uniform size
 channels = [2 6 7 9 11];
 targetLength = 5000; %leghatékonyabb az 5000-es méret
 [SignalsT,LabelsT,SignalsV,LabelsV]=prepOwnData(db,EEG,targetLength,channels);
 
-% parentFolder-t át kell írni egy egy valid mappa elérési címre
-% 1.a - Training data képpé alakítása
-parentFolder = 'C:\Users\Nyírő Balázs\OneDrive - Kormányzati Informatikai Fejlesztési Ügynökség\Egyetem\Önlab\teszt data\v_2';
+% parentFolder must be rewritten to a valid folder path
+% 1.a - Convert Training data to image
+parentFolder = 'C:\Users\Nyírő Balázs\OneDrive - Kormányzati Informatikai Fejlesztési Ügynökség\Egyetem\Önlab\teszt data\v_2'; %modify this folder path
 set_label = '11_10';
 set_name = '_Train';
 childFolder = append(set_label,set_name);
-%spektogram reprezentáció morlet wavelet segítségével
+% spectrogram representation using morlet wavelet
 SPECTfromTF(SignalsT,LabelsT,parentFolder,childFolder); 
 
-% 1.b - Beolvas datastore-ba a könyebb kezelés érdekében
+% 1.b - Scan to datastore for easier management
 imgsTrain = imageDatastore(fullfile(parentFolder,childFolder),...
     'IncludeSubfolders',true,...
     'LabelSource','foldernames');
 clc
 
-% ugyan ezt a validációs adatokkal is megcsináljuk
-%2.a - Validation data képpé alakítása
+% we do the same with the validation data
+%2.a - Convert Validation data to image
 set_name = '_Valid';
 childFolder = append(set_label,set_name);
 SPECTfromTF(SignalsV,LabelsV,parentFolder,childFolder);
@@ -91,15 +88,15 @@ clc
 
 %% GoogLeNet
 
-% GoogLeNet előre tanított háló betöltése 
+% Load GoogLeNet pre-trained mesh 
 net = googlenet;
 
-% GoogLeNet Network paramétereinek megváltoztatása
-% 1. Legutolsó droput layer helyettesítése egy 0.6-os értékű dropout layerrel
+% Change GoogLeNet Network parameters
+% 1. Replace the last dropout layer with a dropout layer of 0.6
 % New dropout
-% Layer Graph kinyerése. Ha a neurális háló egy SeriesNetwork object,
-% mint pl AlexNet, VGG-16 vagy a VGG-19, akkor konvertálja a list of layers-t
-% net.Layers-t egy layer graph.
+% Retrieve Layer Graph. If the neural network is a SeriesNetwork object,
+% such as AlexNet, VGG-16 or VGG-19, then convert the list of layers
+% net.Layers to a layer graph.
 if isa(net,'SeriesNetwork') 
   lgraph = layerGraph(net.Layers); 
 else
@@ -110,7 +107,7 @@ newDropoutLayer = dropoutLayer(0.6,'Name','new_Dropout');
 lgraph = replaceLayer(lgraph,'pool5-drop_7x7_s1',newDropoutLayer);
 
 
-% 2. Fully connected layer cserélése egy olyanra ahol 3 class van (ahány kategóriánk a labelekben)
+% 2. Replace Fully connected layer with a layer with 3 classes (as many categories in the labels)
 numClasses = numel(categories(imgsTrain.Labels));
 
 [learnableLayer,classLayer] = findLayersToReplace(lgraph);
@@ -131,25 +128,25 @@ end
 lgraph = replaceLayer(lgraph,learnableLayer.Name,newConnectedLayer);
 
 
-% 3. Az eredeti classification layer kicserélése egy olyanra amiben nincsenek
-%    label-ek. Ezeket majd a betanítás során autómatikusan beállítja magának.
+% 3. Replace the original classification layer with one that does not
+% contain labels. These will be set automatically during training.
 newClassLayer = classificationLayer('Name','new_classoutput');
 lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
 
-% Háló struktúra megtekintés (pcionális)
+% View of mesh structure(optional)
 % figure('Units','normalized','Position',[0.3 0.3 0.4 0.4]);
 % plot(lgraph)
 % ylim([0,10])
 
-% Freeze(azokat a rétegeket melyek az alapvető struktúrák feliserését végezik
-% befagyasztjuk, azaz ezeknek az értékeit nem változtatja majd a betanulás során) 
+% Freeze(the layers that are used to lift the basic structures
+% will be frozen, i.e. their values will not change during the learning process) 
 layers = lgraph.Layers;
 connections = lgraph.Connections;
 
 layers(1:10) = freezeWeights(layers(1:10));
 lgraph = createLgraphUsingConnections(layers,connections);
 
-% Training Options beállítása az általunk kívánt értékekre
+% Set Training Options to the values you want
 options = trainingOptions('sgdm',...
     'MiniBatchSize',20,...
     'MaxEpochs',20,...
@@ -161,27 +158,28 @@ options = trainingOptions('sgdm',...
     'Plots','training-progress');
 rng default
 
-%% Háló betanítása
-% training futás elindítása
+%% Training
+% Start training run
 trainedGN = trainNetwork(imgsTrain,lgraph,options);
 
-%% Pontosság kiszámolása
-% pontosság számolása validációs adatokra
+%% Calculation of accuracy
+% Calculation of accuracy for validation data
 trainedGN.Layers(end)
 
 [YPred,probs] = classify(trainedGN,imgsValidation);
 accuracy = mean(YPred==imgsValidation.Labels);
 disp(['GoogLeNet Accuracy: ',num2str(100*accuracy),'%'])
 
-%% Klasszifikálás - teszt
+%% Classification - test
 
-% validation képekre --> random válasz 4-et a validációs képek közül
-% majd ezeket klasszifikálja. Ha ezt helyesen teszi az adott szakasz
-% spektogramja felett zöld felirat jelzi hogy melyik szakasz lett
-% megállapítva és mekkora pontossággal. Ha nem találta el a felirat piros
-% illetve megjelenik hogy mi a valódi besorolása az adott szakasznak
+% validation images --> random answer 4 of the validation images
+% then classifies them. If this is done correctly the given section
+% spectrogram of a given section, a green label above the 
+% spectrogram indicates which section has been
+% identified and with what accuracy. If not found, the label is red
+% and the actual classification of the given section is displayed
 
-% a klasszifikálás hosszát mérjük
+% the length of the classification is measured
 tic
 [YPred,probs] = classify(trainedGN,imgsValidation);
 toc
